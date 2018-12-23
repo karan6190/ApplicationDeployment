8a1e0ca41643a0ac73e2b1e44b4fa8779c1c78dc
karan6190 <karansharma6190@gmail.com>
2018-12-24 00:59:36 +0530

    updated security group

--- modules/beanstalk/eb_securtiy.tf
+++ modules/beanstalk/eb_securtiy.tf
@@ -32,7 +32,13 @@ resource "aws_security_group" "beanstalk-SG" {
       to_port = 443
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-  }  
+  }
+  ingress {
+      from_port = 5678
+      to_port = 5678
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }   
 tags {
     Name = "${var.VPC_NAME}-beanstalk-${var.appname}-${var.ENV}-SG"
   }
