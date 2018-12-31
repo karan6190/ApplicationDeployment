fbbeff0cca0f329bfb4a42e04a9dd7da89fb7da5
Karan Sharma <karansharma6190@gmail.com>
2018-12-31 20:31:20 +0530

    Updated Zone ID

--- deploy-templates/dev/route53.tf
+++ deploy-templates/dev/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "www" {
-  zone_id = "ZT4ZLDIG6GVW2"
+  zone_id = "ZTxxxxxxx"
   name    = "${var.VPC_NAME}-${var.ENV}-${var.appname}"
   type    = "CNAME"
   ttl     = "300"
